f9cccbc30e8ab7d82ff7aa09d7fe72fc8d988917
amanuelcm27 <amanuelfirew27@gmail.com>
2024-12-18 02:24:50 +0300

    setting up folder structure

--- addis_book/app/_layout.jsx
+++ addis_book/app/_layout.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App to starfldsjlajft x on your app!</Text>
+      <Text>Open up App to start on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
